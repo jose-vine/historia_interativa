d3c561b8937e469bb05dbc55acde35ae2f99cb78
José Vine <josevine729@gmail.com>
2023-12-27 10:16:09 -0300

    Adicionando imagem à primeira página de história interativa

--- js/scriptHistoria.js
+++ js/scriptHistoria.js
@@ -1,5 +1,5 @@
 const usuario = JSON.parse(localStorage.getItem('usuarioLogado'));
 
-const divTelaHistoria = document.querySelector('#telaHistoria');
+const paragrafoHistoria = document.querySelector('#paragrafoHistoria');
 
-divTelaHistoria.innerHTML = `<p>Seja bem-vindo(a), ${usuario.nome}! Tudo bem com você? Espero que sim!</p><br><p>Nas próximas páginas, embarcaremos em uma jornada que vai gerar uma história temática com base nas informações preenchidas durante o seu cadastro. Ela vai contar com um pouco de diversão e lições de moral importantes que vão servir de incentivo ao usuário. Vamos lá?</p>`;
+paragrafoHistoria.innerHTML = `Seja bem-vindo(a), ${usuario.nome}! <br>Tudo bem com você? Espero que sim!</p><br><p>Nas próximas páginas, embarcaremos em uma jornada que vai gerar uma história temática com base nas informações preenchidas durante o seu cadastro. <br>Ela vai contar com um pouco de diversão e lições de moral importantes que vão servir de incentivo ao usuário. Vamos lá?`;
