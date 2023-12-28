44cf3576b8c9b4dabc02ac77a860b946f99c74b7
José Vine <josevine729@gmail.com>
2023-12-27 22:28:13 -0300

    Aprimoramento da primeira página da história interativa

--- js/scriptHistoria.js
+++ js/scriptHistoria.js
@@ -1,5 +1,7 @@
 const usuario = JSON.parse(localStorage.getItem('usuarioLogado'));
 
 const paragrafoHistoria = document.querySelector('#paragrafoHistoria');
+const mensagemBoasVindas = document.querySelector('#mensagemBoasVindas')
 
-paragrafoHistoria.innerHTML = `Seja bem-vindo(a), ${usuario.nome}! <br>Tudo bem com você? Espero que sim!</p><br><p>Nas próximas páginas, embarcaremos em uma jornada que vai gerar uma história temática com base nas informações preenchidas durante o seu cadastro. <br>Ela vai contar com um pouco de diversão e lições de moral importantes que vão servir de incentivo ao usuário. Vamos lá?`;
+mensagemBoasVindas.innerHTML = `Seja bem-vindo(a), ${usuario.nome}!`;
+paragrafoHistoria.innerHTML = `Tudo bem com você? Espero que sim!</p><br><p>Nas próximas páginas, embarcaremos em uma jornada que vai gerar uma história temática com base nas informações preenchidas durante o seu cadastro. <br><br>Ela vai contar com um pouco de diversão e lições de moral importantes que vão servir de incentivo ao usuário. Vamos lá?`;
