f10e952ed156a5359b28bcfab7faee06cb8eafc3
José Vine <josevine729@gmail.com>
2024-01-02 16:25:12 -0300

    Inserindo geração aleatória das cartas

--- jogo-da-memoria/js/script.js
+++ jogo-da-memoria/js/script.js
@@ -1,9 +1,50 @@
-const containerCartas = document.querySelector('#cartas');
+const container = document.querySelector('#container');
+const cores = [
+    'azul',
+    'azul',
+    'vermelho',
+    'vermelho',
+    'verde',
+    'verde',
+    'preto',
+    'preto',
+    'laranja',
+    'laranja',
+    'roxo',
+    'roxo',
+    'cinza',
+    'cinza',
+    'amarelo',
+    'amarelo'
+];
 
-const criarElemento = (tag, classe) => {
+function criarElemento(tag, classe) {
     const elemento = document.createElement(tag);
     elemento.className = classe;
     return elemento;
 }
 
-criarElemento('div', 'carta');
+function adicionarFilho(pai, filho) {
+    pai.appendChild(filho);
+}
+
+for (let i = 0; i < 16; i++) {
+    const indiceAleatorio = Math.floor(Math.random() * (15-i));
+    const carta = criarElemento('div', 'carta');
+    const flip = criarElemento('div', 'flip');
+    const verso = criarElemento('div', 'verso');
+    const frente = criarElemento('div', cores[indiceAleatorio] + ' frente');
+
+    cores.splice(indiceAleatorio, 1);
+
+    adicionarFilho(carta, flip);
+    adicionarFilho(flip, verso);
+    adicionarFilho(flip, frente);
+
+    if (i % 4 == 0) {
+        var bloco = criarElemento('div', 'bloco');
+        adicionarFilho(container, bloco);
+    }
+
+    adicionarFilho(bloco, carta);
+}
