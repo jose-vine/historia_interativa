896bd4bfcdb331f0e7184c1cbfb8920c85e135cb
José Vine <josevine729@gmail.com>
2024-01-02 23:23:52 -0300

    Implementando virada das cartas conforme acertos e erros do usuário

--- jogo-da-memoria/js/script.js
+++ jogo-da-memoria/js/script.js
@@ -28,10 +28,43 @@ function adicionarFilho(pai, filho) {
     pai.appendChild(filho);
 }
 
+let primeiraCarta = '';
+let segundaCarta = '';
+
+function revelarCarta({target}) {
+    if (target.parentNode.className.includes('flip')) {
+        return;
+    }
+
+    if (primeiraCarta == '') {
+        target.parentNode.classList.add('flip');
+        primeiraCarta = target.parentNode;   
+    } else if (segundaCarta == '') {
+        target.parentNode.classList.add('flip');
+        segundaCarta = target.parentNode;
+        
+        if (primeiraCarta.children[1].className != segundaCarta.children[1].className) {
+            setTimeout(() => {
+                primeiraCarta.classList.remove('flip');
+                primeiraCarta.children[1].classList.remove('flip');
+    
+                segundaCarta.classList.remove('flip');
+                segundaCarta.children[1].classList.remove('flip');
+
+                primeiraCarta = '';
+                segundaCarta = '';
+            }, 500);
+        } else {
+            primeiraCarta = '';
+            segundaCarta = '';
+        }
+    }
+}
+
 for (let i = 0; i < 16; i++) {
     const indiceAleatorio = Math.floor(Math.random() * (15-i));
     const carta = criarElemento('div', 'carta');
-    const flip = criarElemento('div', 'flip');
+    const flip = criarElemento('div', '');
     const verso = criarElemento('div', 'verso');
     const frente = criarElemento('div', cores[indiceAleatorio] + ' frente');
 
@@ -47,4 +80,5 @@ for (let i = 0; i < 16; i++) {
     }
 
     adicionarFilho(bloco, carta);
+    carta.addEventListener('click', revelarCarta);
 }
